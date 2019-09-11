058b8a2cb5cb502154f17e043b1345a30dd5e1c9
yahu1031 <35523357+yahu1031@users.noreply.github.com>
2019-09-11 23:48:50 +0530

    Add files via upload

--- registered.php
+++ registered.php
@@ -24,7 +24,7 @@
       body
       {
         font-family: Arial, Helvetica, sans-serif;
-        background-image: url("image/white_texture.png");
+        /* background-image: url("image/white_texture.png"); */
       }
 
       #booking .booking-wrap .search {
@@ -60,6 +60,318 @@
         font-size: 14px;
       }
 
+      </style>
+<!-- Character set configuration -->
+<meta charset="UTF-8">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+<script type="text/javascript" src="http://code.jquery.com/jquery-1.8.2.js"></script>
+<link rel="shortcut icon" type="image/x-icon" href="image/favicon.ico">
+<!-- Google web font  -->
+<link href="https://fonts.googleapis.com/css?family=Oswald:400,500,700|Roboto:300,400,500" rel="stylesheet">
+<!-- Character set configuration -->
+<title>Registration Details</title>
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<!-- Viewport configuration, scaling options -->
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<!-- CSS Stylesheet -->
+<link href="css/bootstrap.css" rel="stylesheet">
+<link href="css/registration-frm-1.css" rel="stylesheet">
+<!-- Font Awesome icon -->
+<link href="css/font-awesome.min.css" rel="stylesheet">
+      <style>
+      @import url('https://fonts.googleapis.com/css?family=Poppins');
+      /* BASIC */
+      body
+      {
+        font-family: "Poppins", sans-serif;
+        height: 100vh;
+      }
+      a
+      {
+        color: #92badd;
+        display:inline-block;
+        text-decoration: none;
+        font-weight: 400;
+      }
+      /* STRUCTURE */
+      .wrapper
+      {
+        display: flex;
+        align-items: center;
+        flex-direction: column;
+        justify-content: center;
+        width: 100%;
+        min-height: 100%;
+        padding: 10px;
+      }
+      #formContent {
+        -webkit-border-radius: 10px 10px 10px 10px;
+        border-radius: 10px 10px 10px 10px;
+        background: #fff;
+        padding: 30px;
+        width: 90%;
+        max-width: 450px;
+        position: relative;
+        padding: 0px;
+        -webkit-box-shadow: 0 30px 60px 0 rgba(0,0,0,0.3);
+        box-shadow: 0 30px 60px 0 rgba(0,0,0,0.3);
+        text-align: center;
+      }
+      #formFooter {
+        background-color: #f6f6f6;
+        border-top: 1px solid #dce8f1;
+        padding: 25px;
+        text-align: center;
+        -webkit-border-radius: 0 0 10px 10px;
+        border-radius: 0 0 10px 10px;
+      }
+      /* FORM TYPOGRAPHY*/
+      input[type=button], input[type=submit], input[type=reset]  {
+        background-color: #56baed;
+        border: none;
+        color: white;
+        padding: 15px 80px;
+        text-align: center;
+        text-decoration: none;
+        display: inline-block;
+        text-transform: uppercase;
+        font-size: 13px;
+        /* -webkit-box-shadow: 0 10px 30px 0 rgba(95,186,233,0.4);
+        box-shadow: 0 10px 30px 0 rgba(95,186,233,0.4); */
+        -webkit-border-radius: 5px 5px 5px 5px;
+        border-radius: 5px 5px 5px 5px;
+        margin: 5px 20px 40px 20px;
+        -webkit-transition: all 0.3s ease-in-out;
+        -moz-transition: all 0.3s ease-in-out;
+        -ms-transition: all 0.3s ease-in-out;
+        -o-transition: all 0.3s ease-in-out;
+        transition: all 0.3s ease-in-out;
+      }
+      input[type=button]:hover, input[type=submit]:hover, input[type=reset]:hover  {
+        background-color: #4CAF50;
+      }
+      input[type=button]:active, input[type=submit]:active, input[type=reset]:active  {
+        -moz-transform: scale(0.95);
+        -webkit-transform: scale(0.95);
+        -o-transform: scale(0.95);
+        -ms-transform: scale(0.95);
+        transform: scale(0.95);
+      }
+      input[type=text], input[type=date], .select {
+        background-color: #f6f6f6;
+        border: none;
+        color: #0d0d0d;
+        padding: 15px 32px;
+        text-align: center;
+        text-decoration: none;
+        display: inline-block;
+        font-size: 16px;
+        margin: 5px;
+        width: 85%;
+        border: 2px solid #f6f6f6;
+        -webkit-transition: all 0.5s ease-in-out;
+        -moz-transition: all 0.5s ease-in-out;
+        -ms-transition: all 0.5s ease-in-out;
+        -o-transition: all 0.5s ease-in-out;
+        transition: all 0.5s ease-in-out;
+        -webkit-border-radius: 5px 5px 5px 5px;
+        border-radius: 5px 5px 5px 5px;
+      }
+      input[type=text]:focus {
+        background-color: #fff;
+        border-bottom: 2px solid #5fbae9;
+      }
+      input[type=text]:placeholder {
+        color: #cccccc;
+      }
+/*password styles*/
+input[type=password] {
+  background-color: #f6f6f6;
+  border: none;
+  color: #0d0d0d;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 5px;
+  width: 85%;
+  border: 2px solid #f6f6f6;
+  -webkit-transition: all 0.5s ease-in-out;
+  -moz-transition: all 0.5s ease-in-out;
+  -ms-transition: all 0.5s ease-in-out;
+  -o-transition: all 0.5s ease-in-out;
+  transition: all 0.5s ease-in-out;
+  -webkit-border-radius: 5px 5px 5px 5px;
+  border-radius: 5px 5px 5px 5px;
+}
+input[type=password]:focus {
+  background-color: #fff;
+  border-bottom: 2px solid #5fbae9;
+}
+input[type=password]:placeholder {
+  color: #cccccc;
+}
+
+      /* ANIMATIONS */
+      /* Simple CSS3 Fade-in-down Animation */
+      .fadeInDown {
+        -webkit-animation-name: fadeInDown;
+        animation-name: fadeInDown;
+        -webkit-animation-duration: 1s;
+        animation-duration: 1s;
+        -webkit-animation-fill-mode: both;
+        animation-fill-mode: both;
+      }
+      @-webkit-keyframes fadeInDown {
+        0% {
+          opacity: 0;
+          -webkit-transform: translate3d(0, -100%, 0);
+          transform: translate3d(0, -100%, 0);
+        }
+        100% {
+          opacity: 1;
+          -webkit-transform: none;
+          transform: none;
+        }
+      }
+      @keyframes fadeInDown {
+        0% {
+          opacity: 0;
+          -webkit-transform: translate3d(0, -100%, 0);
+          transform: translate3d(0, -100%, 0);
+        }
+        100% {
+          opacity: 1;
+          -webkit-transform: none;
+          transform: none;
+        }
+      }
+      /* Simple CSS3 Fade-in Animation */
+      @-webkit-keyframes fadeIn { from { opacity:0; } to { opacity:1; } }
+      @-moz-keyframes fadeIn { from { opacity:0; } to { opacity:1; } }
+      @keyframes fadeIn { from { opacity:0; } to { opacity:1; } }
+      .fadeIn {
+        opacity:0;
+        -webkit-animation:fadeIn ease-in 1;
+        -moz-animation:fadeIn ease-in 1;
+        animation:fadeIn ease-in 1;
+        -webkit-animation-fill-mode:forwards;
+        -moz-animation-fill-mode:forwards;
+        animation-fill-mode:forwards;
+        -webkit-animation-duration:1s;
+        -moz-animation-duration:1s;
+        animation-duration:1s;
+      }
+      .fadeIn.first {
+        -webkit-animation-delay: 0.4s;
+        -moz-animation-delay: 0.4s;
+        animation-delay: 0.4s;
+      }
+      .fadeIn.second {
+        -webkit-animation-delay: 0.6s;
+        -moz-animation-delay: 0.6s;
+        animation-delay: 0.6s;
+      }
+      .fadeIn.third {
+        -webkit-animation-delay: 0.8s;
+        -moz-animation-delay: 0.8s;
+        animation-delay: 0.8s;
+      }
+      .fadeIn.fourth {
+        -webkit-animation-delay: 1s;
+        -moz-animation-delay: 1s;
+        animation-delay: 1s;
+      }
+      /* Simple CSS3 Fade-in Animation */
+      .underlineHover:after {
+        display: block;
+        left: 0;
+        bottom: -10px;
+        width: 0;
+        height: 2px;
+        background-color: #56baed;
+        content: "";
+        transition: width 0.2s;
+      }
+      .underlineHover:hover {
+        color: #0d0d0d;
+      }
+      .underlineHover:hover:after{
+        width: 100%;
+      }
+.pass
+{
+  input: password;
+}
+      /* OTHERS */
+      *:focus {
+          outline: none;
+      }
+      #icon {
+        width:60%;
+      }
+      * {
+        box-sizing: border-box;
+      }
+      .banner {
+    position: absolute;
+    z-index: 9999;
+    padding: .75rem 1.25rem;
+    margin-bottom: 1rem;
+    border: 1px solid transparent;
+    border-radius: .25rem;
+    display: none;
+}
+
+.banner.active {
+    display: block;
+}
+
+.banner-bottom {
+    left: 0;
+    right: 0;
+    bottom: 10px;
+}
+
+.banner-top {
+    left: 0;
+    right: 0;
+    top: 10px;
+}
+
+.banner-right {
+    right: 10px;
+    bottom: 10%;
+    min-height: 10vh;
+}
+
+.banner-left {
+    left: 10px;
+    bottom: 10%;
+    min-height: 10vh;
+}
+
+.alert-primary {
+    color: #004085;
+    background-color: #cce5ff;
+    border-color: #b8daff;
+}
+
+.banner-close {
+    position: absolute;
+    right: 1.5%;
+}
+
+.banner-close:after {
+    position: absolute;
+    right: 0;
+    top: 50%;
+    content: 'X';
+    color: #69f;
+}
       </style>
 </head>
 <body>
@@ -98,8 +410,103 @@
   </div>
     </nav>
     </section>
+    <form method="POST">
+<center>
+<div id="formContent">
+        <br><br>
+        <h2><b> Book Tickets </b></h4>
+        <br><br>
+        <!-- Login Form -->
+        <select id="from" size="1" onchange="makeSubmenu(this.value)" class="select" name="from" required onclick="filterCity()" required="">
+                      <option value="" disabled="" selected="">From</option>
+                      <option value="NGOsColony"> NGOs Colony </option>
+                      <option value="PanamaGodown"> Panama Godown </option>
+                      <option value="LBNagar"> LB Nagar </option>
+                      <option value="Kothapet"> Kothapet </option>
+                      <option value="Chaitanyapuri"> Chaitanyapuri </option>
+                      <option value="Dilsukhnagar"> Dilsukhnagar </option>
+                      <option value="Mosarambagh"> Mosarambagh </option>
+                      <option value="SuperBazar"> Super Bazar </option>
+                      <option value="NalgondaXRoads"> Nalgonda X Roads </option>
+                      <option value="Malakpet"> Malakpet </option>
+                      <option value="Chaderghat"> Chaderghat </option>
+                      <option value="Womenscollege"> Womens college </option>
+                      <option value="Koti"> Koti Bus Stop </option>
+                      <option value="RamKoti"> Ram Koti </option>
+                      <option value="Badichowdi"> Badichowdi </option>
+                      <option value="MadapatiHanumanthaSchool"> Madapati Hanumantha School </option>
+                      <option value="YMCA"> YMCA(Koti) Bus Stop </option>
+                      <option value="Narayanguda"> Narayanguda </option>
+                      <option value="Chikkadpally"> Chikkadpally </option>
+                      <option value="RTC"> RTC Cross Rd </option>
+                      <option value="GolcondaXRoads"> Golconda X Roads </option>
+                      <option value="SapthagiriTheater"> Sapthagiri Theater </option>
+                      <option value="RajaDelux"> Raja Delux </option>
+                      <option value="MusheerabadBusStop"> Musheerabad Bus Stop </option>
+                      <option value="JailGarden"> Jail Garden </option>
+                      <option value="GandhiHospital"> Gandhi Hospital </option>
+                      <option value="PadmaraoNagar"> Padmarao Nagar </option>
+                      <option value="Punjab"> Bhoiguda </option>
+                      </select>
+                      <select id="to" size="1" onchange="makeSubmenu(this.value)" class="select" name="to" required onclick="filterCity()" required="">
+                      <option value="" disabled="" selected="">To</option>
+                      <option value="NGOsColony"> NGOs Colony </option>
+                      <option value="PanamaGodown"> Panama Godown </option>
+                      <option value="LBNagar"> LB Nagar </option>
+                      <option value="Kothapet"> Kothapet </option>
+                      <option value="Chaitanyapuri"> Chaitanyapuri </option>
+                      <option value="Dilsukhnagar"> Dilsukhnagar </option>
+                      <option value="Mosarambagh"> Mosarambagh </option>
+                      <option value="SuperBazar"> Super Bazar </option>
+                      <option value="NalgondaXRoads"> Nalgonda X Roads </option>
+                      <option value="Malakpet"> Malakpet </option>
+                      <option value="Chaderghat"> Chaderghat </option>
+                      <option value="Womenscollege"> Womens college </option>
+                      <option value="Koti"> Koti Bus Stop </option>
+                      <option value="RamKoti"> Ram Koti </option>
+                      <option value="Badichowdi"> Badichowdi </option>
+                      <option value="MadapatiHanumanthaSchool"> Madapati Hanumantha School </option>
+                      <option value="YMCA"> YMCA(Koti) Bus Stop </option>
+                      <option value="Narayanguda"> Narayanguda </option>
+                      <option value="Chikkadpally"> Chikkadpally </option>
+                      <option value="RTC"> RTC Cross Rd </option>
+                      <option value="GolcondaXRoads"> Golconda X Roads </option>
+                      <option value="SapthagiriTheater"> Sapthagiri Theater </option>
+                      <option value="RajaDelux"> Raja Delux </option>
+                      <option value="MusheerabadBusStop"> Musheerabad Bus Stop </option>
+                      <option value="JailGarden"> Jail Garden </option>
+                      <option value="GandhiHospital"> Gandhi Hospital </option>
+                      <option value="PadmaraoNagar"> Padmarao Nagar </option>
+                      <option value="Punjab"> Bhoiguda </option>
+                      <option value="SecunderabadJunction"> Secunderabad Junction </option>
+                      </select>
+          <input type="date" id="doj" name="doj" placeholder="DD/MM/YYYY" required="">
+          <input type="submit" method="POST" name="submit" value="Regsiter">
 
-  
+      </div>
+</center>
+  </form>
+  <?php
+  if(isset($_POST["submit"]))
+  {
+    $from = $_POST['from'];
+    $to = $_POST['to'];
+    $doj = $_POST['doj'];
+    if($from == $to)
+    {
+        echo "<script>alert('from and destination cannot be same!!!')</script>";
+    }
+    else
+    {
+    $sql="INSERT INTO `tickets`(`from`, `to`, `doj`)
+          VALUES ('$from','$to','$doj')";
+    $result=$mysqli->query($sql);
+    echo $sql;
+    if($result)
+    echo "<script>alert('ticket booked !!')</script>";
+    }
+  }
+?>
     <script>
 
     </script>
